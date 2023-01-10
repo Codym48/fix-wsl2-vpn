823f186c8e29b617aadf4fd208bafa7137ad6b34
Cody Martin <codytylermartin@gmail.com>
2023-01-10 15:55:55 -0700

    Get WSL IP and subnet mask from powershell cmdlet

--- fix.cmd
+++ fix.cmd
@@ -20,17 +20,32 @@ rem 64 bytes from 8.8.8.8: icmp_seq=1 ttl=115 time=10.7 ms
 rem 64 bytes from 8.8.8.8: icmp_seq=2 ttl=115 time=11.0 ms
 rem 64 bytes from 8.8.8.8: icmp_seq=3 ttl=115 time=10.4 ms
 rem ...
+rem
+rem Desirements
+rem - Doesn't require installing any dependencies (no python, etc.)
+rem - Completely self-contained in a single file
 
-set wsl_adapter_ip=172.21.0.0
-set wsl_adapter_mask=255.255.240.0
+rem Assumes a single network whose alias contains "WSL"
+for /f "delims=" %%i in (
+    ' powershell -NonInteractive -NoProfile -Command "(Get-NetIPConfiguration -InterfaceAlias *WSL*).IPv4Address.IPAddress" '
+) do set "wsl_adapter_ip=%%i"
+rem We need to delete the route from .0 even though ipconfig returns .1
+set "wsl_adapter_ip=%wsl_adapter_ip:~0,-1%0"
 
-rem This works if there's a single network adapter whose description starts with PANGP*
+rem Use the PrefixLength to calculate the subnet mask via binary math
+rem (Assume subnet mask is PrefixLength 1s followed by 32-PrefixLength 0s)
+rem https://networkengineering.stackexchange.com/questions/7106/how-do-you-calculate-the-prefix-network-subnet-and-host-numbers
+for /f "delims=" %%i in (
+    ' powershell -NonInteractive -NoProfile -Command "[int]$prefix_length = (Get-NetIPConfiguration -InterfaceAlias *WSL*).IPv4Address.PrefixLength; [int]$prefix_rem = $prefix_length; [int]$ones = (0,(8,$prefix_rem | Measure -Min).Minimum | Measure -Max).Maximum; [int]$first = [Convert]::ToInt16('1'*$ones+'0'*(8-$ones),2); [int]$prefix_rem = $prefix_rem-8; [int]$ones = (0,(8,$prefix_rem | Measure -Min).Minimum | Measure -Max).Maximum; [int]$second = [Convert]::ToInt16('1'*$ones+'0'*(8-$ones),2); [int]$prefix_rem = $prefix_rem-8; [int]$ones = (0,(8,$prefix_rem | Measure -Min).Minimum | Measure -Max).Maximum; [int]$third = [Convert]::ToInt16('1'*$ones+'0'*(8-$ones),2); [int]$prefix_rem = $prefix_rem-8; [int]$ones = (0,(8,$prefix_rem | Measure -Min).Minimum | Measure -Max).Maximum; [int]$fourth = [Convert]::ToInt16('1'*$ones+'0'*(8-$ones),2); $out = $first,$second,$third,$fourth -join '.'; echo $out" '
+) do set "wsl_adapter_mask=%%i"
+
+rem Assumes a single network adapter whose description starts with "PANGP"
 for /f "delims=" %%i in (
     ' powershell -NonInteractive -NoProfile -Command "(Get-NetAdapter -InterfaceDescription PANGP*).ifIndex" '
 ) do set "vpn_adapter_index=%%i"
 
 :loop
-call route delete %wsl_adapter_ip% mask %wsl_adapter_mask% 0.0.0.0 ^
- IF %vpn_adapter_index% > route-delete-result.txt 2>&1
+call route delete %wsl_adapter_ip% mask %wsl_adapter_mask% 0.0.0.0 IF %vpn_adapter_index% > route-delete-result.txt 2>&1
 set /p result=<route-delete-result.txt
+timeout /t 1 > nul
 if "%result%"==" OK!" goto loop
